78865664ae9f4150f90e15475c177baf8bc2ab0b
majkeleq <graf3rpl@gmail.com>
2023-04-12 10:53:57 +0200

    Added functionality for button SentenceCase

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -15,3 +15,12 @@ document.getElementById("proper-case").addEventListener("click", function() {
         .map( word => word.charAt(0).toUpperCase().concat(word.substring(1, word.length)));
     textArea.value = words.join(" ").trim();
 });
+
+document.getElementById("sentence-case").addEventListener("click", function() {
+    let textArea = document.querySelector("textarea");
+    let sentences = textArea.value
+        .split(".")
+        .map(sentence => sentence.trim())
+        .map(sentence => sentence.charAt(0).toUpperCase().concat(sentence.substring(1, sentence.length).toLowerCase()));
+    textArea.value = sentences.join(". ").trim();
+});
