007f34db9c7aa629d209e44c50fbd135d1710aa2
majkeleq <graf3rpl@gmail.com>
2023-04-12 12:52:58 +0200

    Stage 3/3. Added Save Text Button

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -24,3 +24,21 @@ document.getElementById("sentence-case").addEventListener("click", function() {
         .map(sentence => sentence.charAt(0).toUpperCase().concat(sentence.substring(1, sentence.length).toLowerCase()));
     textArea.value = sentences.join(". ").trim();
 });
+
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+document.getElementById("save-text-file").addEventListener("click", function () {
+    let textArea = document.querySelector("textarea");
+    download("text.txt", textArea.value)
+})
